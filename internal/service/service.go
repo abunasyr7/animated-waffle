73414922df20f4679fd3755e10a23050f734a31b
Abunasyr <abunasyr1@gmail.com>
2026-06-24 12:22:46 +0500

    feat: add function to define if string Morse

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"strings"
+)
+
+func isMorse(s string) bool {
+	s = strings.TrimSpace(s)
+	if len(s) == 0 {
+		return false
+	}
+
+	for _, token := range strings.Fields(s) {
+		if token == "" {
+			continue
+		}
+
+		for _, r := token {
+			if r != '.' && r != '-' {
+				return false
+			} 
+		}
+	}
+
+	return true
+}
